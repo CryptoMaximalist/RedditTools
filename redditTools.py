27c508299bd2adb486952dfeeeb749fcf7d08124
CryptoMaximalist <46038551+CryptoMaximalist@users.noreply.github.com>
2019-06-19 16:21:37 -0400

    Add appendWiki, buildLogList, and verifyHeaders

--- redditTools.py
+++ redditTools.py
@@ -66,16 +66,83 @@ class redditTools:
         try:
             wikiPage = reddit.subreddit(subreddit).wiki[page]
             if returnFormat == 'list':
-                data_list = [y for y in (x.strip() for x in wikiPage.content_md.splitlines()) if y]
+                data_list = [y for y in (x.strip() for x in wikiPage.content_md.splitlines()) if y] # Split lines into list
                 return(data_list)
             else:
                 return(wikiPage.content_md)
         except Exception as e:
             print("There was a problem fetching wiki page's contents: "+str(e))
+
         
     def writeWiki(subreddit, page, newData, reddit):
         wikiPage = reddit.subreddit(subreddit).wiki[page]
         try:
-            wikiPage.edit(newData) 
+             wikiPage.edit(newData)
         except Exception as e:
             print("There was a problem writing to wiki page: "+str(e))
+
+
+    def appendWiki(subreddit, page, newData, reddit):
+        oldWikiData = []
+        separatorString = ''
+        
+        try:
+            wikiPage = reddit.subreddit(subreddit).wiki[page]
+            oldWikiData = [y for y in (x.strip() for x in wikiPage.content_md.splitlines()) if y] # Split lines into list
+        except Exception as e:
+            print("There was a problem fetching wiki page's contents: "+str(e))
+        
+        newWikiData = oldWikiData.copy()
+        newWikiData.append(newData)
+        newWikiData = '\n'.join(newWikiData)
+        try:
+            print('Committing to wiki')
+            wikiPage.edit(newWikiData)
+        except Exception as e:
+            print("There was a problem writing to wiki page: "+str(e))
+
+
+    def buildLogList(log):
+        linkList = ['removelink', 'approvelink', 'removecomment', 'approvecomment', 'distinguish', 'spamlink', 'spamcomment', 'sticky', 'ignorereports', 'markoriginalcontent']
+        detailsList = ['editsettings', 'wikirevise']
+        authorList = ['banuser', 'unbanuser']
+        logList = []
+        logList.append(str(int(log.created_utc)))
+        logList.append(str(log.subreddit))
+        logList.append(str(log.mod))
+        logList.append(str(log.action))
+        if log.action in linkList:
+            logList.append(str(log.target_permalink))
+        elif log.action in detailsList:
+            logList.append(str(log.details))
+        elif log.action in authorList:
+            logList.append(str(log.target_author))
+        elif log.action == 'editflair':
+            if log.target_permalink is None:
+                logList.append(str(log.target_permalink))
+            else:
+                logList.append(str(log.target_author))
+        else:
+            print()
+            print('ERROR: Unknown Action type: '+log.action)
+            print(vars(log))
+        return(logList)
+
+        
+    def verifyHeaders(headers):
+        #Get old wiki data and write new data
+        #Unfinished
+        pipedHeaders = '|'.join(headers)
+        for i in headers:
+            separatorString = separatorString+'|-'
+        separatorString = separatorString[1:]
+        #print(pipedHeaders)
+        #print(separatorString)
+        #print(oldWikiData[0])
+        print(oldWikiData)
+        print(len(oldWikiData))
+        if (len(oldWikiData) < 2) or (oldWikiData[0] != pipedHeaders) or (oldWikiData[1] != separatorString):
+            print('Prepending headers')
+            oldWikiData.insert(0,separatorString)
+            oldWikiData.insert(0,pipedHeaders)
+        
